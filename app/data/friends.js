11a9e59bbccc06c653d0adc01177143ac308de4f
nnien001 <nnien001@gmail.com>
2017-01-11 02:12:11 -0800

    Fixed api-routes to have friend finding logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,9 +3,13 @@ console.log("using friends.js");
 //dummy data. New data goes into this var.
 var friends = [
 	{
-		name: "NattNien",
-		photo: "https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=150",
+		name: "yesman",
+		photo: "http://vignette3.wikia.nocookie.net/fallout/images/b/be/Yes_Man_default.png/revision/latest/scale-to-width-down/185?cb=20110501151202",
 		scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+	}, {
+		name: "grumpycat",
+		photo: "http://i2.kym-cdn.com/photos/images/newsfeed/000/406/325/b31.jpg",
+		scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 	}
 ];
 
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -11,8 +11,54 @@ router.get("/friends", function (req, res) {
 router.post("/friends", function (req, res) {
   var newFriend = req.body;
   console.log(newFriend);
+
+  var bestMatchIndex = 0;
+  var bestMatchSame = 0;
+  var bestMatchDiff = 0;
+
+  //loop through all our friends
+  for(var i = 0; i < friends.length; i++) {
+
+  	var samePoint = 0;
+  	var diffPoint = 0;
+  	
+  	//iterate through all the scores, counting answers that match and taking diffs of answers that don't
+  	console.log("comparing vs ", friends[i].name);
+  	console.log(newFriend.scores);
+  	console.log(friends[i].scores);
+
+  	for(var j = 0; j < friends[i].scores.length; j++) {
+
+  		if (parseInt(friends[i].scores[j]) === parseInt(newFriend.scores[j])) {
+  			samePoint++;
+  		}
+  		else if(parseInt(friends[i].scores[j]) > parseInt(newFriend.scores[j]) ) {
+  			diffPoint = diffPoint + (parseInt(friends[i].scores[j]) - parseInt(newFriend.scores[j]));
+  		}
+  		else {
+  			diffPoint = diffPoint + (parseInt(newFriend.scores[j]) - parseInt(friends[i].scores[j]));	
+  		}
+  	}
+
+  	console.log("samePoint", samePoint);
+  	console.log("diffPoint", diffPoint);
+
+  	if( (samePoint > bestMatchSame) || (samePoint === bestMatchSame && diffPoint < bestMatchDiff)) {
+  		bestMatchIndex = i;
+  		bestMatchSame = samePoint;
+  		bestMatchDiff = diffPoint;
+  	}
+
+  }
+
+  //add your newfriend to the list of friends
   friends.push(newFriend);
-  res.json(newFriend);
+
+  console.log(friends[bestMatchIndex]);
+  console.log(bestMatchIndex, bestMatchSame, bestMatchDiff);
+
+  //return the best match
+  res.json(friends[bestMatchIndex]);
 
 });
 
